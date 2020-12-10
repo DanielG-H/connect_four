c02dd098572534b3cd0b85d6899c84247e8b4b3b
DanielG-H <danielgarcia.alumno@gmail.com>
2020-12-10 11:01:35 -0800

    chore: add program files and gitignore

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace connect_four
+{
+    class Game_logic{
+
+        public  Game_logic(){
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game_logic new_game = new Game_logic();
+            Console.ReadLine();
+        }
+    }
+}
